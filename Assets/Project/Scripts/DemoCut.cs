a682c549d50f324d5146ddbd17dd0f5a7c5fe86f
Swing Wren <swingwren@gmail.com>
2019-09-05 20:19:51 +0200

    first commit

---
+++ Assets/Project/Scripts/DemoCut.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Spine;
+using Spine.Unity;
+using UnityEngine;
+
+public class DemoCut : MonoBehaviour {
+  [SerializeField] private SkeletonGraphic skeletonGraphic;
+
+  private void Start() {
+    TrackEntry trackEntry = skeletonGraphic.AnimationState.SetAnimation(0, "animation", true);
+    trackEntry.AnimationStart = (float) 80 / 30;
+    trackEntry.AnimationEnd = (float) 120 / 30;
+  }
+}
